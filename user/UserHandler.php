7070e1bc85554360276a8134b1fc71097d6910c3
Sean Finch <fench@hauntedbees.com>
2016-09-04 16:46:17 -0700

    included commonFunctions.php

--- user/UserHandler.php
+++ user/UserHandler.php
@@ -1,6 +1,7 @@
 <?php
 session_start();
 require_once $_SERVER["DOCUMENT_ROOT"]."/bq/SqlManager/SqlManager.php";
+require_once $_SERVER["DOCUMENT_ROOT"]."/bq/common/commonFunctions.php";
 class UserHandler {
 	private $sql;
 	function __construct() { $this->sql = new SQLManager(); }
