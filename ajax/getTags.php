2a99b4627201e2387db0013463dac0d440c9a360
Sean Finch <fench@hauntedbees.com>
2016-09-05 00:13:21 -0700

    improvements to that guy.!

--- ajax/getTags.php
+++ ajax/getTags.php
@@ -4,16 +4,22 @@ require_once $_SERVER["DOCUMENT_ROOT"]."/bq/base/BasePage.php";
 require_once $_SERVER["DOCUMENT_ROOT"]."/bq/base/Validation.php";
 $userId = ValidateAndReturnUserId(true);
 
-$tag = array_pop(explode(" ", trim($_GET["term"])));
+$tagList = trim($_GET["term"]);
+$tags = explode(" ", $tagList);
+$tag = array_pop($tags);
 $sql = new SQLManager();
 $canAdd = $sql->QueryCount("SELECT iLevel FROM bq_users WHERE cID = :user", ["user" => $userId]) >= 3;
 $results = [];
+$alreadyExists = false;
 if($tag != "") {
 	$tagTbl = $sql->Query("SELECT sTag FROM bq_tags WHERE sTag LIKE :tag ORDER BY sTag LIMIT 0, 5", ["tag" => "$tag%"]);
-	while($row = $tagTbl->fetch(PDO::FETCH_ASSOC)) { $results[] = ["id" => $row["sTag"], "label" => $row["sTag"], "value" => $row["sTag"]]; }
+	while($row = $tagTbl->fetch(PDO::FETCH_ASSOC)) { 
+		if($row["sTag"] == $tag) { $alreadyExists = true; }
+		$results[] = ["id" => $row["sTag"], "label" => $row["sTag"], "value" => $row["sTag"]];
+	}
 }
 if($canAdd) {
-	$results[] = ["id" => $tag, "label" => "Create tag '$tag.'", "value" => $tag];
+	if(!$alreadyExists) { $results[] = ["id" => $tag, "label" => "Create tag '$tag.'", "value" => $tag]; }
 } else {
 	$results[] = ["id" => "", "label" => "You must be level 3 or higher to add your own tags.", "value" => ""];
 }
