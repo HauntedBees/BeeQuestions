c59f7478b3883a0da53b369ab7aad92ce78c2326
Sean Finch <fench@hauntedbees.com>
2016-09-04 19:57:10 -0700

    iconv seems like a good idea.

--- base/Validation.php
+++ base/Validation.php
@@ -10,9 +10,29 @@ function QStoDB($val, $errMsg) { // converts querystring value to binary value t
 function WordFilterAndRemoveHTML($s) {
 	$badWords = ["poopoo", "doodoo"];
 	$badRegexes = ["/\sjerks?/"];
-	$replacements = ["a" => "@|4", "b" => "8", "c" => "\(|\[", "e" => "3", "f" => "ph", "g" => "6",
-					 "o" => "0", "q" => "9", "s" => "$|5", "t" => "7", "w" => "vv", "z" => "2"];
-	$halfreplacements = ["" => "", "c" => "k", "i" => "l|!|1", "k" => "c", "l" => "i|!|1", "u" => "v", "v" => "u"];
+	$s = iconv("UTF-8", "ISO-8859-1//TRANSLIT", $s);
+	$replacements = ["a" => "@|4|À|Á|Â|Ã|Ä|Å|à|á|â|ã|ä|å|ª",
+					 "b" => "8|ß", 
+					 "c" => "\(|\[|Ç|ç|¢|©",
+					 "d" => "ð|Ð",
+					 "e" => "3|³|È|É|Ê|Ë|è|é|ê|ë", 
+					 "f" => "ph",
+					 "g" => "6",
+					 "i" => "Ì|Í|Î|Ï|ì|í|î|ï|¹|¡",
+					 "l" => "£", 
+					 "n" => "Ñ|ñ", 
+					 "o" => "0|º|Ò|Ó|Ô|Õ|Ö|Ø|ò|ó|ô|õ|ö|ø|¤|°", 
+					 "p" => "¶", 
+					 "q" => "9", 
+					 "r" => "®",
+					 "s" => "§", 
+					 "t" => "7|±", 
+					 "u" => "µ|Ù|Ú|Û|Ü|ù|ú|û|ü", 
+					 "w" => "vv", 
+					 "x" => "×", 
+					 "y" => "Ý|Ÿ|ý|ÿ",
+					 "z" => "2|²"];
+	$halfreplacements = ["" => "", "a" => "Æ|æ", "b" => "þ|Þ", "c" => "k", "e" => "Æ|æ", "i" => "l|!|1", "k" => "c", "l" => "i|!|1", "p" => "þ|Þ", "u" => "v", "v" => "u"];
 	$s2 = strtolower($s);
 	$s2 = preg_replace("/-/", "", $s2);
 	$s2 = preg_replace("/[^a-z0-9]/", " ", $s2);
@@ -33,7 +53,7 @@ function ReturnError($msg) {
 function ValidateAndReturnUserId($isAjax=false) {
 	if(!isset($_SESSION["fbid"])) {
 		if($isAjax) { echo json_encode(["status" => false, "errorMessage" => "Please log in."]); }
-		else { header("Location: http://".$_SERVER["SERVER_NAME"]."/bq/index.php?errno=4200"); }
+		else { header("Location: http://".$_SERVER["SERVER_NAME"]."/bq/index.html?errno=4200"); }
 		exit;
 	}
 	$c = parse_ini_file($_SERVER["DOCUMENT_ROOT"]."/bq/secure/config.ini", true)["facebook"];
@@ -46,7 +66,7 @@ function ValidateAndReturnUserId($isAjax=false) {
 	if($_SESSION["fbid"] != $accessToken->getValue()) {
 		unset($_SESSION["fbid"]);
 		if($isAjax) { echo json_encode(["status" => false, "errorMessage" => "An error has occurred. Please log in again to proceed."]); }
-		else { header("Location: http://".$_SERVER["SERVER_NAME"]."/bq/index.php?errno=4201"); }
+		else { header("Location: http://".$_SERVER["SERVER_NAME"]."/bq/index.html?errno=4201"); }
 		exit;
 	}
 	try {
@@ -54,12 +74,12 @@ function ValidateAndReturnUserId($isAjax=false) {
 	} catch(Facebook\Exceptions\FacebookResponseException $e) {
 		unset($_SESSION["fbid"]);
 		if($isAjax) { echo json_encode(["status" => false, "errorMessage" => "An error has occured. Please log in again to proceed."]); }
-		else { header("Location: http://".$_SERVER["SERVER_NAME"]."/bq/index.php?errno=4201"); }
+		else { header("Location: http://".$_SERVER["SERVER_NAME"]."/bq/index.html?errno=4201"); }
 		exit;
 	} catch(Facebook\Exceptions\FacebookSDKException $e) {
 		unset($_SESSION["fbid"]);
 		if($isAjax) { echo json_encode(["status" => false, "errorMessage" => "An error has occured. Please log in again to proceed."]); }
-		else { header("Location: http://".$_SERVER["SERVER_NAME"]."/bq/index.php?errno=4201"); }
+		else { header("Location: http://".$_SERVER["SERVER_NAME"]."/bq/index.html?errno=4201"); }
 		exit;
 	}
 
@@ -70,7 +90,7 @@ function ValidateAndReturnUserId($isAjax=false) {
 	if($userID == null || $userID <= 0) {
 		unset($_SESSION["fbid"]);
 		if($isAjax) { echo json_encode(["status" => false, "errorMessage" => "An error has occured. Please log in again to proceed."]); }
-		else { header("Location: http://".$_SERVER["SERVER_NAME"]."/bq/index.php?errno=4201"); }
+		else { header("Location: http://".$_SERVER["SERVER_NAME"]."/bq/index.html?errno=4201"); }
 		exit;
 	}
 	$banDate = $userInfo["dtBannedUntil"];
@@ -78,7 +98,7 @@ function ValidateAndReturnUserId($isAjax=false) {
 		$banDateTime = new DateTime($banDate);
 		if($banDateTime > new DateTime()) {
 			if($isAjax) { echo json_encode(["status" => false, "errorMessage" => "You are currently banned from performing this action."]); }
-			else { header("Location: http://".$_SERVER["SERVER_NAME"]."/bq/index.php?err=4202"); }
+			else { header("Location: http://".$_SERVER["SERVER_NAME"]."/bq/index.html?err=4202"); }
 			exit;
 		}
 	}
