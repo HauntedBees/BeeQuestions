f60c56cabb5aa2cd7b613f34a5cbfa352ab303a7
Sean Finch <fench@hauntedbees.com>
2016-09-04 19:53:31 -0700

    prevented viewing other peoples' histories.

--- ajax/getUserHistory.php
+++ ajax/getUserHistory.php
@@ -4,5 +4,9 @@ require_once $_SERVER["DOCUMENT_ROOT"]."/bq/base/BasePage.php";
 require_once $_SERVER["DOCUMENT_ROOT"]."/bq/common/userFunctions.php";
 $page = new BasePage("users/user.html");
 $userId = $page->sql->QueryVal("SELECT cID FROM bq_users WHERE bnID = :id", ["id" => hex2bin(Base64::toHex($_POST["user"]))]);
+if(!isset($page->userInfo["id"]) || $page->userInfo["id"] != $userId) { 
+	echo "You can't view other people's history!";
+	exit;
+}
 echo GetUserHistory($page, new SQLManager(), $userId, $_POST["filter"], $_POST["offset"]);
 ?>
