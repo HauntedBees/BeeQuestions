b5438daa3edcfa4c9e10b5d7b67f299d40c6b471
Sean Finch <fench@hauntedbees.com>
2016-09-11 23:49:46 -0700

    man there were a lot of mistakes in this logic lol

--- ajax/chooseQuestion.php
+++ ajax/chooseQuestion.php
@@ -15,13 +15,13 @@ $answer64 = $answerInfo["cID64"];
 
 $sql->Query("UPDATE bq_answers SET iStatus = 3, dtClosed = NOW(), xBestQuestion = :q WHERE cID = :a", ["a" => $answerId, "q" => $questionId]);
 
-$questionData = $sql->QueryRow("SELECT q.xAnswer, q.sQuestion, a.sAnswer, u.cID64 AS uID64 FROM bq_questions q INNER JOIN bq_answers a ON q.xAnswer = a.cID INNER JOIN bq_users u ON q.xUser = u.cID WHERE q.cID = :questionId", ["questionId" => $questionId]);
+$questionData = $sql->QueryRow("SELECT q.xAnswer, q.sQuestion, a.sAnswer, u.cID AS uID, u.cID64 AS uID64 FROM bq_questions q INNER JOIN bq_answers a ON q.xAnswer = a.cID INNER JOIN bq_users u ON q.xUser = u.cID WHERE q.cID = :questionId", ["questionId" => $questionId]);
 $query = <<<EOT
 	INSERT INTO bq_notifications (xUser, sTemplate, sIconClass, sToken1, sToken2, sToken3, sToken4, dtPosted, bDismissed) VALUES 
 		(:them, 'yourBestQuestion.html', 'glyphicon-question-sign', :aURL, :a, :qURL, :q, NOW(), 0)
 EOT;
 $sql->Query($query, [
-	"them" => $questionData["uID64"], 
+	"them" => $questionData["uID"], 
 	"aURL" => "http://hauntedbees.com/bq/answers/$answer64", 
 	"a" => $questionData["sAnswer"],
 	"qURL" => "http://hauntedbees.com/bq/answers/$answer64#q".$_POST["id"], 
