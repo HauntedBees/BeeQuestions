a9d176fea532f499a911a5ad06c8956d9990bdfa
Sean Finch <fench@hauntedbees.com>
2016-09-11 23:58:33 -0700

    nyoro~n :3c

--- common/scoreUpdating.php
+++ common/scoreUpdating.php
@@ -4,7 +4,7 @@ function IncrementScore($sql, $userId, $amount) {
 	$oldScore = intval($OldScoreAndLevel["iScore"]);
 	$newScore = $oldScore + $amount;
 	$oldLevel = intval($OldScoreAndLevel["iLevel"]);
-	$newLevel = $sql->QueryVal("SELECT MAX(iLevel) FROM bq_levels WHERE :newScore > iScoreRequired", ["newScore" => $newScore]);
+	$newLevel = $sql->QueryVal("SELECT MAX(iLevel) FROM bq_levels WHERE :newScore >= iScoreRequired", ["newScore" => $newScore]);
 	$sql->Query("UPDATE bq_users SET iScore = :newScore, iLevel = :newLevel WHERE cID = :user", ["user" => $userId, "newScore" => $newScore, "newLevel" => $newLevel]);
 	return ["pchange" => $amount, "lchange" => ($newLevel - $oldLevel), "level" => $newLevel];
 }
